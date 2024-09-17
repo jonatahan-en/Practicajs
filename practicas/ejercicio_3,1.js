79ef5853f462c56e116a669c48cf416ba9cd2baa
jonatahan-en <jonatahan_enriquez@hotmail.com>
2024-09-17 20:13:00 +0300

    Tercer ejercicio de la practica

---
+++ practicas/ejercicio_3,1.js
@@ -0,0 +1,35 @@
+/*
+Crea una función para que con estos datos de entrada se produzca los siguientes
+*/
+
+
+const input1 = [
+'Downloads',
+'Videos',
+'capture',
+'mp4',
+]
+
+const getInput = (list) => {
+    const getElemet = list.length -1;extension = list[getElemet]; newList = [...list];newList.pop();
+    return (`Resultado ejercicio 3.1: ${newList.join('/')}.${extension}`)
+};
+
+console.log(getInput(input1)); // 'Downloads/Videos/capture.mp4'
+const input2 = [
+'CodinGame',
+'python',
+'py',
+];
+console.log(getInput(input2)); // 'CodinGame/python.py'
+const input3 = [
+'programming',
+'languages',
+'easy',
+'beginner',
+'useful',
+'pythonstuff',
+'py',
+]
+console.log(getInput(input3));
+// 'programming/languages/easy/beginner/useful/pythonstuff.py
