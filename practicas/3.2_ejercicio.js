2783389a2653ef7583fb3eeb99c32bdb85f30679
jonatahan-en <jonatahan_enriquez@hotmail.com>
2024-09-17 20:37:07 +0300

    Cuarto ejercicio 3.2,y refactorizacion

---
+++ practicas/3.2_ejercicio.js
@@ -0,0 +1,12 @@
+/*
+Crea una función para que con estos datos de entrada se produzca los siguientes
+*/
+const input = 10;
+const changeNumbers = (number) =>{string = number.toString();
+    return(`Resultado ejercicio 3.2 :${string.split('').join('-')}`);
+};
+console.log(changeNumbers(input)); // '1-0'
+const secondInput = 1;
+console.log(changeNumbers(secondInput)); // '1'
+const thirdInput = 11234;
+console.log(changeNumbers(thirdInput)); // '1-1-2-3-4'
