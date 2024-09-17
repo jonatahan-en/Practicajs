90e596347a134225cf09c6e56956618329c03657
jonatahan-en <jonatahan_enriquez@hotmail.com>
2024-09-17 20:53:16 +0300

    Quinto ejercicio 3.3

---
+++ practicas/3.3_ejrcicio.js
@@ -0,0 +1,19 @@
+/*
+Crea una función para que con estos datos de entrada se produzca los siguientes
+*/
+
+
+
+const input1 = 'string';
+
+const longReverse = (item) => {
+    const characteres = item.length;
+    const newlist = [...item];
+    const invest = newlist.reverse();
+    return(`Resultado ejrcicio 3.3: ${characteres}  ${invest.join('')}`)
+}
+console.log(longReverse(input1)); // '6 gnirts'
+const input2 = 'variable';
+console.log(longReverse(input2)); // '8 elbairav'
+const input3 = 'pointer';
+console.log(longReverse(input3)); // '7 retniop'
