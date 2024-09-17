a08635dd2ca19fcdb116952e73226ce5f98ff36c
jonatahan-en <jonatahan_enriquez@hotmail.com>
2024-09-17 19:28:49 +0300

    refactorizacion

--- practicas/buj.js
+++ practicas/buj.js
@@ -7,7 +7,7 @@ const calcularPromedio = (numeros) => {
 let sumaTotal = 0;
 for (let i = 0; i < numeros.length; i++) {
     sumaTotal += numeros[i];
-}
+};
 const promedio = sumaTotal / numeros.length;
 return promedio;
 };
