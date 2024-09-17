41594aacbe16b05068f0f03b182ab872cbfb59aa
jonatahan-en <jonatahan_enriquez@hotmail.com>
2024-09-17 19:26:42 +0300

    Segundo archivo de la practica,buj.js

---
+++ practicas/buj.js
@@ -0,0 +1,23 @@
+/*
+Nuestro cliente está intentando calcular el promedio de una lista de números pero nos dice
+que no funciona. No nos da el error, solo este código que es el que tiene en producción.
+Para este ejercicio tenemos que crear un archivo llamado bug.js con la solución.
+*/
+const calcularPromedio = (numeros) => {
+let sumaTotal = 0;
+for (let i = 0; i < numeros.length; i++) {
+    sumaTotal += numeros[i];
+}
+const promedio = sumaTotal / numeros.length;
+return promedio;
+};
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
+console.log(`El promedio es: ${promedioNumeros}`)
+
+
+/**
+ * Existen dos errores:
+ * 1.  En el for ,añadio  un signo = demas.
+ * 2. En el retorno de la funcion no se hace el consol.log
+ */
